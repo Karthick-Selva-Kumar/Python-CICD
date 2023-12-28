b9529352cb2142df5dfb9484149bd16982fbf8c0
Karthick <151127150+Karthick-Selva-Kumar@users.noreply.github.com>
2023-12-28 13:51:17 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull karthickselvakumar/mypythonapp
+docker pull karthickselvakumar1999/mypythonapp
 
 # Run the Docker image as a container
-docker run -d --name mypythoncontainer1 -p 5001:5000 karthickselvakumar/mypythonapp
+docker run -d --name mypythoncontainer1 -p 5001:5000 karthickselvakumar1999/mypythonapp
